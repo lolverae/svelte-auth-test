3d0d82fdbd2dbc57ec9bff53836ceb5b2713b96c
lolverae <alberto.olvera.espinosa@gmail.com>
2024-05-10 22:24:48 -0600

    Add user info to page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,8 @@
 <div class="p-20">
   {#if $page.data.session}
     <h1>Logged in</h1>
+    <h2>Hello {$page.data.session.user?.name}</h2>
+      <img alt="User Profile" src={$page.data.session.user?.image} />
   {:else}
     <h1>Not logged in</h1>
     <button on:click={() => signIn("github")}>Sign In</button>
