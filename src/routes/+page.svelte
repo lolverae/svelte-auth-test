69204b1c3f9a400c1289bcb1dbeb5977d8fc9ae9
lolverae <alberto.olvera.espinosa@gmail.com>
2024-05-11 22:48:30 -0600

    Add prettier login buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,11 +21,13 @@
     </button>
   {:else}
     <h1 class="text-3xl font-bold text-blue-500 mb-4">Not logged in</h1>
-    <button type="button" class="btn btn-secondary bg-blue-100 rounded-full text-lg p-2" on:click={() => signIn("github")}>
-      Sign In with GitHub
+    <button class="px-4 py-2 border flex gap-2 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400  hover:text-slate-900 hover:shadow transition duration-150" on:click={() => signIn("github")}>
+      <img class="w-6 h-6" src="https://www.svgrepo.com/show/361181/github.svg" loading="lazy" alt="github logo">
+      <span>Login with Github</span>
     </button>
-    <button type="button" class="btn btn-secondary bg-blue-100 rounded-full text-lg p-2" on:click={() => signIn("google")}>
-      Sign In with Google
+    <button class="px-4 py-2 border flex gap-2 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400  hover:text-slate-900 hover:shadow transition duration-150" on:click={() => signIn("google")}>
+      <img class="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo">
+      <span>Login with Google</span>
     </button>
   {/if}
 </div>
